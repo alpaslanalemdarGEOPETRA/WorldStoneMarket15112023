dc1cb0991848c3df013c69c20d586f8f39da0027
alpaslanalemdarGEOPETRA <alpaslanalemdar5406@hotmail.com>
2023-11-15 23:57:34 +0300

    Staleelement issue solved. Try catch applied properly. New webelement list are created in every cycle, so that stale issue not happened anymore
    -1

--- src/test/java/StepDefinitions/WSM_TargetWebsiteWebScrapingAndExcellBuildUp.java
+++ src/test/java/StepDefinitions/WSM_TargetWebsiteWebScrapingAndExcellBuildUp.java
@@ -25,20 +25,14 @@ public class WSM_TargetWebsiteWebScrapingAndExcellBuildUp {
     @When("Go and Collect data as a Array list")
     public void goAndCollectDataAsAArrayList() throws InterruptedException {
 
-        for (int i = 1; i <= 3; i++) { // 10 sayfa için döngü
-            // Sayfa numarasına tıklama işlemi
-
-            //  driver.get("https://www.stonecontact.com/suppliers/turkey-country/" + i);
+        for (int i = 1; i <=2 ; i++) {                      // i PAGE CYCLE
             driver.get("https://stoneexpozone.com/companiesdb/1/tr?country=tr&type=1&pg=" + i);
-            Map<String, String> dataCollection = null;
 
 
-            String originalHandle = driver.getWindowHandle();
 
             List<WebElement> companyProfiles =dc.getWebElementList("companyProfilesOnListingPage");
 
             for (int j = 0; j < companyProfiles.size(); j++) {
-                // Her eleman için sayfayı yeniden yüklemeden önce yeniden bul
                 companyProfiles =dc.getWebElementList("companyProfilesOnListingPage");
                 WebElement ele = companyProfiles.get(j);
 
@@ -65,28 +59,7 @@ public class WSM_TargetWebsiteWebScrapingAndExcellBuildUp {
 
 
 
-                //   wait.until(ExpectedConditions.numberOfWindowsToBe(2));
 
-                //    for (String handle : driver.getWindowHandles()) {
-                //        if (!handle.equals(originalHandle)) {
-                //            driver.switchTo().window(handle);
-//
-                //        //  String companyName=ele.getText();
-                //        //   String companyCountry=driver.findElement(By.xpath("(//div[@class='profile-info-line companyinfo-item']/text())[3]")).getText();
-                //        //   System.out.println("companyCountry = " + companyCountry);
-                //        //   dataCollection.add(companyName,companyCountry);
-                //
-//
-                //            // Veri toplama işlemleri burada
-                //            // Örnek: collectedData.add(driver.findElement(By.id("someId")).getText());
-//
-                //            // Yeni sekme kapatılıyor
-                //            driver.close();
-                //            // Kontrolü orijinal sekme (liste sayfası) için geri al
-                //            driver.switchTo().window(originalHandle);
-                //            break;
-                //        }
-                //    }
             }
         }
 
