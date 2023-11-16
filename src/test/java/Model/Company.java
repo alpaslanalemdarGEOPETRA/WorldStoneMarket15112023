bcc770cfbabeb3e9ab60f9532793d676dac81b83
alpaslanalemdarGEOPETRA <alpaslanalemdar5406@hotmail.com>
2023-11-16 18:50:53 +0300

    Model created for HashMap, HashMap build and was put into tryCatch properly.

---
+++ src/test/java/Model/Company.java
@@ -0,0 +1,40 @@
+package Model;
+
+public class Company {
+    private String description;
+    private String country;
+    private String companyType;
+    private String companyAddress;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCompanyType() {
+        return companyType;
+    }
+
+    public void setCompanyType(String companyType) {
+        this.companyType = companyType;
+    }
+
+    public String getCompanyAddress() {
+        return companyAddress;
+    }
+
+    public void setCompanyAddress(String companyAddress) {
+        this.companyAddress = companyAddress;
+    }
+}
--- src/test/java/StepDefinitions/WSM_TargetWebsiteWebScrapingAndExcellBuildUp.java
+++ src/test/java/StepDefinitions/WSM_TargetWebsiteWebScrapingAndExcellBuildUp.java
@@ -1,5 +1,6 @@
 package StepDefinitions;
 
+import Model.Company;
 import Pages.DialogContent;
 import Pages.LeftNav;
 import Utilities.GWD;
@@ -10,6 +11,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -25,29 +27,47 @@ public class WSM_TargetWebsiteWebScrapingAndExcellBuildUp {
     @When("Go and Collect data as a Array list")
     public void goAndCollectDataAsAArrayList() throws InterruptedException {
 
-        for (int i = 1; i <=2 ; i++) {                      // i PAGE CYCLE
+        Map<String, Company> companyMap = new HashMap<>();
+
+
+        for (int i = 1; i <= 2; i++) {                      // i PAGE CYCLE
             driver.get("https://stoneexpozone.com/companiesdb/1/tr?country=tr&type=1&pg=" + i);
 
 
-
-            List<WebElement> companyProfiles =dc.getWebElementList("companyProfilesOnListingPage");
+            List<WebElement> companyProfiles = dc.getWebElementList("companyProfilesOnListingPage");
 
             for (int j = 0; j < companyProfiles.size(); j++) {
-                companyProfiles =dc.getWebElementList("companyProfilesOnListingPage");
+                companyProfiles = dc.getWebElementList("companyProfilesOnListingPage");
                 WebElement ele = companyProfiles.get(j);
+                Company company = new Company();
 
 
-                System.out.println("ele.getText() = " + ele.getText());
+
+              //  System.out.println("ele.getText() = " + ele.getText());
 
 
                 try {
                     dc.myClick(ele);
+
+                    company.setDescription(driver.findElement(By.xpath("//div[@class='span-11 last']")).getText());
+                    company.setCompanyAddress(driver.findElement(By.xpath("//p[@itemprop=\"address\"]")).getText());
+                  //  System.out.println("company.getDescription() = " + company.getDescription());
+                  //  System.out.println("company.getCompanyAddress() = " + company.getCompanyAddress());
+
+
                     driver.navigate().back();
 
                 } catch (StaleElementReferenceException st) {
-                    companyProfiles =dc.getWebElementList("companyProfilesOnListingPage");
+                    companyProfiles = dc.getWebElementList("companyProfilesOnListingPage");
                     ele = companyProfiles.get(j);
                     dc.myClick(ele);
+
+                    company.setDescription(driver.findElement(By.xpath("//div[@class='span-11 last']")).getText());
+                    company.setCompanyAddress(driver.findElement(By.xpath("//p[@itemprop=\"address\"]")).getText());
+                  //  System.out.println("company.getDescription() = " + company.getDescription());
+                  //  System.out.println("company.getCompanyAddress() = " + company.getCompanyAddress());
+
+
                     driver.navigate().back();
 
                 } catch (WebDriverException we) {
@@ -55,14 +75,26 @@ public class WSM_TargetWebsiteWebScrapingAndExcellBuildUp {
                 }
 
 
-               // THE PLACE OF DATA COLLECTION FROM WEBELEMENTS IN COMPANY PROFILES
+                companyMap.put(ele.getText(), company);
+              //  Company company = new Company();
 
+             //  try {
 
+             //      company.setDescription(driver.findElement(By.xpath("//div[@class='span-11 last']")).getText());
+             //      company.setCompanyAddress(driver.findElement(By.xpath("//p[@itemprop=\"address\"]")).getText());
+             //      System.out.println("company.getDescription() = " + company.getDescription());
+             //      System.out.println("company.getCompanyAddress() = " + company.getCompanyAddress());
+             //  }catch (StaleElementReferenceException sx){
+             //
+             //
+             //  }
+                // THE PLACE OF DATA COLLECTION FROM WEBELEMENTS IN COMPANY PROFILES
 
 
             }
         }
 
+        System.out.println("companyMap = " + companyMap);
     }
 
     @Then("Write it into an excell")
